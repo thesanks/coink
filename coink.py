26aeace7b6c2fa3120441395d6efe565103c8dec
br0b0t <dazzlerlogue@gmail.com>
2018-05-27 20:44:02 +0000

    fixed form to display selected currencies after submit

--- coink.py
+++ coink.py
@@ -6,18 +6,18 @@ app = Flask(__name__)
 
 URL = "https://openexchangerates.org//api/latest.json?app_id=3792aac73c0d4da982217fcfbaf18cce"
 
-DEFAULTS = { 'currency_have':'GBP','currency_to':'USD' }
+DEFAULTS = { 'currency_have':'GBP','currency_want':'USD' }
 
 @app.route("/")
 def home():
     currency_have = request.args.get("currency_have")
     if not currency_have:
         currency_have = DEFAULTS['currency_have']
-    currency_to = request.args.get("currency_to")
-    if not currency_to:
-        currency_to = DEFAULTS['currency_to']
-    rate, currencies = get_rate(currency_have, currency_to)
-    return render_template("index.html", currency_have=currency_have, currency_to=currency_to, rate=rate, currencies=sorted(currencies))
+    currency_want = request.args.get("currency_want")
+    if not currency_want:
+        currency_want = DEFAULTS['currency_want']
+    rate, currencies = get_rate(currency_have, currency_want)
+    return render_template("index.html", currency_have=currency_have, currency_want=currency_want, rate=rate, currencies=sorted(currencies))
 
 def get_rate(have, want):
     r = requests.get(URL)
