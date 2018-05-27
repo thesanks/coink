93f2ccf79dde90a479f2c30b31ebb72d4d7a39f3
br0b0t <dazzlerlogue@gmail.com>
2018-05-27 21:10:00 +0000

    changed index and get_rate so it returns converted quantity amount

--- coink.py
+++ coink.py
@@ -6,18 +6,21 @@ app = Flask(__name__)
 
 URL = "https://openexchangerates.org//api/latest.json?app_id=3792aac73c0d4da982217fcfbaf18cce"
 
-DEFAULTS = { 'currency_have':'GBP','currency_want':'USD' }
+DEFAULTS = { 'currency_have':'GBP', 'currency_want':'USD', 'quantity': 100}
 
 @app.route("/")
 def home():
+    quantity = request.args.get("quantity")
+    if not quantity:
+        currency_have = DEFAULTS['quantity']
     currency_have = request.args.get("currency_have")
     if not currency_have:
         currency_have = DEFAULTS['currency_have']
     currency_want = request.args.get("currency_want")
     if not currency_want:
         currency_want = DEFAULTS['currency_want']
-    rate, currencies = get_rate(currency_have, currency_want)
-    return render_template("index.html", currency_have=currency_have, currency_want=currency_want, rate=rate, currencies=sorted(currencies))
+    conversion, currencies = get_rate(currency_have, currency_want)
+    return render_template("index.html", currency_have=currency_have, currency_want=currency_want, conversion=conversion, currencies=sorted(currencies))
 
 def get_rate(have, want):
     r = requests.get(URL)
@@ -25,7 +28,8 @@ def get_rate(have, want):
     rates = r['rates']
     have = rates[have.upper()]
     want = rates[want.upper()]
-    return (want / have, rates.keys())
+    quantity = request.args.get("quantity")
+    return (int(quantity) * (want / have), rates.keys())
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080, debug=True)
